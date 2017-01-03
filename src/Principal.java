d1e3ee905fdb0d01fd218cebf791502119b4dec0
odacil <odacilpaul@gmail.com>
2017-01-03 13:42:48 +0000

    Done

--- src/Principal.java
+++ src/Principal.java
@@ -3,8 +3,8 @@ public class Principal {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("hola bebe");
 		System.out.println("tudo ");
+		
 	}
 
 }
